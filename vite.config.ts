ac3499c31a499c5bfcbfb47e3508aefe6977875e
Hamdan Haneefa <hamdanhaneefa23@gmail.com>
2025-08-15 12:45:41 +0530

    changes docs error

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/BestowCreditLink/',
+  build: {
+    outDir: 'docs' 
+  },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react']
   },
   esbuild: {
     loader: 'jsx',
-    include: /src\/.*\.[jt]sx?$/,
-  },
-});
+    include: /src\/.*\.[jt]sx?$/
+  }
+})
