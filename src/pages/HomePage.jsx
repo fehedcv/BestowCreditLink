840d84c8201f8341bca10a622756d0ad8c5986b2
Fahad Mohammed Kabeer Chullivalappil <110394736+fehedcv@users.noreply.github.com>
2025-08-23 20:57:36 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -144,7 +144,7 @@ const HomePage = () => {
               </h1>
               <p className="text-xl mb-8 text-blue-100 max-w-lg">
                 We offer Home loan, NRI loans, Vehicle loan, Personal loan,
-                Business loan, Agriculture loan,Loan against Property, OD, Gold loan and more.
+                Business loan, Agriculture loan, Loan against Property, OD, Gold loan and more.
               </p>
               <div className="flex flex-col sm:flex-row gap-4">
                 <a href="/contact">
